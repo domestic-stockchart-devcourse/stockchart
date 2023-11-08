987bcd0a3af03fdadaccde89bd506080e2a67c6a
yein-lee <agop0310@gmail.com>
2023-11-08 11:25:53 +0900

    feat: create db model

---
+++ app/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+
+
+class Article(models.Model):
+    headline = models.CharField(max_length=200)
+    crawling_time = models.DateTimeField()
+
+    def __str__(self):
+        return self.headline
+
+
+class StockPrice(models.Model):
+    stock_name = models.CharField(max_length=100)
+    date = models.DateField()
+    time = models.TimeField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.stock_name} - {self.date} {self.time}"
+
+
+class TradingVolume(models.Model):
+    stock_name = models.CharField(max_length=100)
+    current_price = models.DecimalField(max_digits=10, decimal_places=2)
+    previous_close_price = models.DecimalField(max_digits=10, decimal_places=2)
+    volume_increase_rate = models.DecimalField(max_digits=5, decimal_places=2)
+    time = models.TimeField()
+
+    def __str__(self):
+        return f"{self.stock_name} - {self.time}"
